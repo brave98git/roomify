a70cc74052385bb3c9706ea09782f60890cd1838
Samarth <samarthpawarsamarthpawar@gmail.com>
2026-02-21 14:17:46 +0530

    first Commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import {Box} from "lucide-react";
+import Button from "./ui/Button";
+import {useOutletContext} from "react-router";
+
+const Navbar = () => {
+    const {isSignedIn,username,signIn,signOut} = useOutletContext<AuthContext>()
+    const handleAuthClick = async () => {
+        if(isSignedIn){
+            try{
+                await signOut();
+            }catch (e){
+                console.error("Puter Sign Out Failed");
+            }
+        }
+
+        try{
+            await signIn();
+        }catch (e){
+            console.error("Puter Login Failed");
+        }
+        return;
+    }
+
+    return (
+        <header className="navbar">
+            <nav className="inner">
+                <div className="left">
+                    <div className="brand">
+                        <Box className="logo" />
+                        <span className="name">Roomify</span>
+                    </div>
+                    <ul className="links">
+                        <a href="#">Product</a>
+                        <a href="#">Pricing</a>
+                        <a href="#">Community</a>
+                        <a href="#">Enterprise</a>
+                    </ul>
+                </div>
+                <div className="actions">
+                    {isSignedIn? (
+                        <>
+                            <span className="greeting">
+                                {username ? `Hi ${username}` : `Signed in`}
+                            </span>
+
+                            <Button size="sm" onClick={handleAuthClick} className="btn">
+                                Log Out
+                            </Button>
+
+                        </>
+                        ): (
+                        <>
+                          <Button size="sm" variant="ghost" onClick={handleAuthClick}>
+                           Log In
+                          </Button>
+                            <a href="#upload" className="cta">Get Started</a>
+                        </>
+
+                        )}
+
+                </div>
+            </nav>
+        </header>
+    )
+}
+export default Navbar
